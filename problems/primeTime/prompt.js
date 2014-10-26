4ffd36cd90fd7f5f0e2929069c329591033071f4
Morgan Plant <morgantheplant@gmail.com>
2014-10-26 13:09:12 -0700

    (test) add prime number test

---
+++ problems/primeTime/prompt.js
@@ -0,0 +1,18 @@
+/*
+* A prime number is a natural number greater than 1   
+* that has no positive divisors other that 1 and itself.
+* 
+* Write a function that given N returns an array of the first 
+* N prime numbers.
+* 
+* for instance
+*
+* primeTime(1) = [2]
+* primeTime(2) = [2, 3]
+* primeTime(5) = [2, 3, 5, 7, 11]
+* primeTime(10) = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+*/
+
+var primeTime = function(n) {
+
+};
---
+++ problems/primeTime/test.js
@@ -0,0 +1,30 @@
+// this function returns a number from 0 to 100
+// indicating what percentage of tests were passed
+exports.testFunction = function(evenFib) {
+	var testsPassed = 0;
+	var totalTests = 5;
+
+    var checkAry = function(prime, actualAry, length){  
+      var result = true;
+      if(prime.length !== length){
+      	return !result;
+      }
+      
+      prime.forEach(function(itm, index, collection){
+         if(itm !== actualAry[index]) {
+         	result = false;
+         }
+      });
+      return result;
+    }
+
+	testsPassed = (checkAry(primeTime(1), [2], 1)) +
+								(checkAry(primeTime(4), [2,3,5,7], 4)) +
+								(checkAry(primeTime(10), [2,3,5,7,11,13,17,19,23,29], 10)) +
+								(checkAry(primeTime(6), [2,3,5,7,11,13], 6)) + 
+								(checkAry(primeTime(16), [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59], 16));
+	return (testsPassed / totalTests) * 100;
+};
+
+// in the future, tests.js needs to return feedback about
+// which test passed and which failed
