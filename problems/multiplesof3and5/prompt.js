2e3d424c5c37862de4d7a5fcfe490ed155a5ec5f
radicalsauce <krhale@gmail.com>
2014-10-27 16:01:53 -0700

    Multiples of 3 and 5 prompt

--- problems/multiplesof3and5/prompt.js
+++ problems/multiplesof3and5/prompt.js
@@ -3,6 +3,6 @@
 
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
-var multiplesof3and5 = function() {
+var multiples3and5 = function() {
 
 };
--- problems/multiplesof3and5/test.js
+++ problems/multiplesof3and5/test.js
@@ -1,9 +1,9 @@
 // this function returns a number from 0 to 100
 // indicating what percentage of tests were passed
-exports.testFunction = function(multiplesof3and5) {
+exports.testFunction = function(multiples3and5) {
   var testsPassed = 0;
   var totalTests = 1;
-  testsPassed = (multiplesof3and5() === 234168);
+  testsPassed = (multiples3and5() === 234168);
   return (testsPassed / totalTests) * 100;
 };
 
